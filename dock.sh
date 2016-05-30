f4ff0889e0af8ec583e7a4be0f2689156ef071a4
Adria77 <Adria77@users.noreply.github.com>
2016-05-30 15:29:38 +0200

    Delete dock.sh

--- dock.sh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/bash
-export RBT_ROOT=/home/adria/Escritorio/rDock_2013.1_src
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RBT_ROOT/lib
-export PATH=$PATH:$RBT_ROOT/bin
-rbcavity -r sah_rdock.prm -was -d > sah_cavity.log
-for file in split*sd;do rbdock -i $file -o ${file%%.*}_out -r sah_rdock.prm -p dock.prm -n 20; done
-
